330493331172b265aadab45ad80420e5bce6f95a
Arne Brasseur <arne@arnebrasseur.net>
2014-04-16 11:11:49 +0200

    Add Chain class, basic functional composition of the processors/transformations

---
+++ lib/asset_packager/processor/chain.rb
@@ -0,0 +1,13 @@
+module AssetPackager
+  class Processor
+    class Chain
+      def initialize(processors)
+        @processors = processors
+      end
+
+      def call(doc)
+        @processors.inject(doc) {|doc, proc| proc.(doc) }
+      end
+    end
+  end
+end
---
+++ spec/asset_packager/processor/chain_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe AssetPackager::Processor::Chain do
+  it 'should functionally compose' do
+    chain = described_class.new([
+        ->(x) { x + 1 },
+        ->(x) { x * 5 }
+      ])
+    expect(chain.call(2)).to eq 15
+  end
+end
